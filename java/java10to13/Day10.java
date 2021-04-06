60971a9a926161e4db01770eb42d057c92ea4dc6
pannhapat <pannhapat9@gmail.com>
2021-04-06 15:59:27 +0700

    Day10
    Day10

--- java/java10to13/Day10.java
+++ java/java10to13/Day10.java
@@ -15,6 +15,15 @@ package CTF010248pannhapat;
 public class Day10 {
     
     public static void main(String[] args) {
+        
+        Employee[] employees = new Employee[3];
+        for (int i = 0; i < employees.length; i++){
+            employees[i] = new Employee("name"+i,"lastname"+i,10000,"Position");
+        }
+        System.out.println(String.valueOf(employees[0].firstname));
+        System.out.println(String.valueOf(employees[1].firstname));
+        System.out.println(String.valueOf(employees[2].firstname));
+        
         //12.1    Instance Class ใน Method Main
         Employee dang = new Employee("Dang","Red",10000,"Position A");
         CEO ceo = new CEO("Captain","Marvel",30000,"Position B");
